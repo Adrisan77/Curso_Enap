2bfd055f550b2653b87c9b01b1da45c4b8684ab9
Adrisan77 <drica.san@gmail.com>
2024-12-06 11:51:06 -0300

    Update Lista_Exercício8.py

--- Lista_Exercício8.py
+++ Lista_Exercício8.py
@@ -1,3 +1,44 @@
 import streamlit as st
 st.title("Questão 1")
 st.write("Sou servidor público")
+
+st.divider()
+#Questão 2:
+st.title("Questão 2")
+
+#Aqui vamos usar as formatações de texto padrões do streamlit
+#Eu amo a documentação da página oficial do streamlit, eles trazem vários exemplos e aplicações com imagens:
+#https://docs.streamlit.io/develop/api-reference/write-magic/st.write
+
+st.divider()
+#Questão 2:
+st.title("Questão 2")
+
+#Aqui vamos usar as formatações de texto padrões do streamlit
+#Eu amo a documentação da página oficial do streamlit, eles trazem vários exemplos e aplicações com imagens:
+#https://docs.streamlit.io/develop/api-reference/write-magic/st.write
+
+#Título
+st.title("Este é o título do app")
+
+#Subtítulo
+st.header("Este é o subtítulo")
+
+#Sub-sub-título
+st.subheader("Este é o terceiro subtítulo")
+
+#Texto markdown
+st.markdown("Este é texto")
+
+#Legenda para gráficos
+st.caption("Esta é a a legenda")
+
+#Código
+st.code("x=2021")
+
+#E ainda podemos usar latex!
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+st.divider()
+#Questão 3:
+st.title("Questão 3")
