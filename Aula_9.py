e79f38510d5e0d87a868bf80935f57364f3fae79
Adrisan77 <drica.san@gmail.com>
2025-01-24 19:10:46 -0300

    Update Aula_9.py

--- Aula_9.py
+++ Aula_9.py
@@ -147,3 +147,12 @@ fig_municipios = px.bar(
 
 # exibindo o gráfico no Streamlit
 st.plotly_chart(fig_municipios)
+
+#primeiro passo passo: implementar um slider para que o usuário escolha quantas linhas ele quer visualizar no DataFrame
+numero_de_linhas = st.slider('Quantas linhas você deseja exibir?', min_value = 0, max_value = 100) 
+#Em min_value o valor mínimo que o usuário pode escolher e max_value o valor máximo que o usuário pode escolher
+#aprendemos que o comando df.head() pode receber como parâmetro a quantidade de linhas a serem exibidas
+#aplicamos no df.head: df.head(numero_de_linhas).
+#Para exibir no streamlit não podemos esquecer de usar o st.write. Assim:
+
+st.write(df.head(numero_de_linhas))
