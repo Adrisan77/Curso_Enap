70198b7954c7154ba52aab91d1b3561085df3472
Adrisan77 <drica.san@gmail.com>
2025-01-27 18:26:06 -0300

    Update Aula_10.py

--- Aula_10.py
+++ Aula_10.py
@@ -1,1 +1,9 @@
+#Bibliotecas
+
+import requests
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.warning("Para entender o passo a passo, consulte também o caderno collab "Adriana_Exercícios_Aula10_Encerramento")
 
