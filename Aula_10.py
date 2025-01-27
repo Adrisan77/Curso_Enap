c6a5594ed0f1c693f6951754cc7bf41af63e6fe5
Adrisan77 <drica.san@gmail.com>
2025-01-27 18:27:09 -0300

    Update Aula_10.py

--- Aula_10.py
+++ Aula_10.py
@@ -5,5 +5,5 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.warning("Para entender o passo a passo, consulte também o caderno collab "Adriana_Exercícios_Aula10_Encerramento")
+st.warning('Para entender o passo a passo, consulte também o caderno collab "Adriana_Exercícios_Aula10_Encerramento"')
 
