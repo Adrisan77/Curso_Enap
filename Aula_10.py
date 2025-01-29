3b61a2c3da94041c58bf44381d30240316921cfb
Adrisan77 <drica.san@gmail.com>
2025-01-29 16:38:17 -0300

    Update Aula_10.py

--- Aula_10.py
+++ Aula_10.py
@@ -39,7 +39,7 @@ opcao_sexo = st.selectbox('Selecione o sexo', df_total['Sexo'].unique())
 
 #mostrar o dataframe com o filtro selecionado
 df_filtrado_sexo = df_total[df_total['Sexo'] == opcao_sexo]
-st.header('Deputados do sexo' + opcao)
+st.header('Deputados do sexo' + opcao_sexo)
 st.write(df_filtrado_sexo)
 
 #mostrar o dataframe por estado a partir do que foi filtrado por sexo
