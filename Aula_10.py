45aa066bff0d36b4343c80acade91699c89cfe93
Adrisan77 <drica.san@gmail.com>
2025-01-29 18:17:12 -0300

    Update Aula_10.py

--- Aula_10.py
+++ Aula_10.py
@@ -47,7 +47,7 @@ df_filtrado_sexo = df_total[df_total['Sexo'] == opcao_sexo]
 st.header('Deputados do sexo' + opcao_sexo)
 st.write(df_filtrado_sexo)
 
-df_filtrado_sexo=df_filtrado_sexo['Sexo'].value_counts()
+df_filtrado_sexo = df_filtrado_sexo['Sexo'].value_counts()
 st.write(df_filtrado_sexo)
 
 #mostrar o dataframe por estado a partir do que foi filtrado por sexo
